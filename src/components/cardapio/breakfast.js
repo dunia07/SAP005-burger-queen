b52814ce26c861d56d3dce7678377b33ea04df3b
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-02-23 23:01:22 -0300

    alteração na estrutura do código nos arquivos orderMenu e breakfast

--- src/components/cardapio/breakfast.js
+++ src/components/cardapio/breakfast.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import Button from '../button';
+import Input from '../input';
 //import Button from '../../components/button';
-//import Input from '../input';
 
 const Breakfast = () => {
   const [menuCafe, setMenuCafe] = useState([]);
   const token = localStorage.getItem('userToken') 
-
 
   // const [userCliente, setUserCliente] = useState(''); 
   // const [userMesa, setUserMesa] = useState(''); 
@@ -36,10 +36,56 @@ const Breakfast = () => {
   //   localStorage.getItem('userMesa')
   // }
 
+  const HandleAddPedido = (e) => {
+    e.preventDefault()
+    const product = e.target.parentNode;
+    const idProduct = product.getAttribute('id')
+    const nameProduct = product.getAttribute('name')
+    const priceProduct = product.getAttribute('price')
+
+    const pedido = {
+      id: idProduct,
+      name: nameProduct,
+      price: priceProduct,
+    }
+
+    let resumePedido =[] 
+
+    if (localStorage.hasOwnProperty('resumePedido')) {
+      resumePedido = JSON.parse(localStorage.getItem('resumePedido'))
+    }
+    resumePedido.push({pedido})
+    localStorage.setItem('resumePedido', JSON.stringify(resumePedido))
+
+  }
+
+
+  const [userCliente, setUserCliente] = useState(''); 
+  const [userMesa, setUserMesa] = useState(''); 
+  const [itemPedido, setItemPedido] = useState([]);
+  const [order, setOrder] = useState([])
+
+
+  const cliente = localStorage.setItem('userCliente', userCliente)
+  const mesa = localStorage.setItem('userMesa', userMesa)
+  const nameAtendente = localStorage.getItem('userName')
   
 
+  function handleClick(product) {
+    console.log(product.id);
+    const obj = {
+      id: product.id,
+      qtd:0,
+    }
+       
+    setOrder((prevState) => [...prevState, obj]);
+  }
   
-
+  // setItems(
+  //   items.map((item, index) => {
+  //     item.id === id ? newItem : item
+  //   })
+  // )
   const getProducts = useCallback (() => {
             
     fetch('https://lab-api-bq.herokuapp.com/products/', {
@@ -66,22 +112,8 @@ const Breakfast = () => {
 
   return (
     <div className='product'>
-      
-      <div className='show-product'>      
 
-        {
-          menuCafe.map((product) => {
-            return (
-              <div className='card-product' key={`product-${product.id}`} >            
-                <p className='white-text'>{product.name}</p> 
-                <p className='white-text'>R$ {product.price},00</p> 
-              </div>
-            )
-          })
-        } 
-      </div>
-
-      {/* <div className='show-requests'>
+      <div className='show-input'>
         
         <section>
           <Input
@@ -91,7 +123,7 @@ const Breakfast = () => {
             type='text'
             placeholder='Nome do Cliente'
             value={userCliente}
-            onChange={HandleUserCliente}
+            onChange={(e) => setUserCliente (e.target.value)}
           />
           <Input
             required
@@ -100,77 +132,64 @@ const Breakfast = () => {
             type='text'
             placeholder='Número da Mesa'
             value={userMesa}
-            onChange={HandleUserMesa}
+            onChange={(e) => setUserMesa (e.target.value)}
           />
-        </section> */}
+        </section>
+      </div>
 
-        {/* <p>RESUMO DO PEDIDO</p> */}
+      <div className='show-product'>      
+          {/* className='card-product' key={`product-${product.id}` */}
+        {
+          menuCafe.map((product) => {
+            return (
+              <div className='card-product' id={product.id} name={product.name} price={product.price} 
+                onClick= {HandleAddPedido}>            
+                <p className='white-text'>{product.name}</p> 
+                <p className='white-text'>R$ {product.price},00</p> 
+                {/* <Button 
+                  className='add'
+                  name='+'
+                  type='submit'
+                  onClick= {() => {
+                    console.log('clicou produto')
+                  }}      
+                /> */}
+              </div>
+            )
+          })
+          
+        } 
+      </div>
 
-        { 
-          // itemPedido.length !== 0 &&
-          // itemPedido.map((product, index) => {
+      <div className='show-resume'>  
+      
+        <p>RESUMO DO PEDIDO</p>
+        <p>Atendente: {nameAtendente}</p>
+        <p>Cliente: {userCliente} - Mesa: {userMesa}</p>
+        {/* <p> {localStorage.getItem('product.id')} {localStorage.getItem('product.price')} </p>  */}
 
-          // listResume.push(product.id)
-          // listValue.push(product.price)
-           
-          // const valueTotal = listValue.reduce((valorInicial, valorAdd) => valorInicial + valorAdd, 0)
+        {
+          itemPedido.map((product) => (
 
-          // localStorage.setItem('resume', listResume)
-          // localStorage.setItem('value', valueTotal)
+            
+              <div className='resume' name={product.name} id={product.id} price={product.price}></div>    
+            )
+          )
+        }  
 
-          // return (
-          //   <div className='requests' key={index} onClick={() => HandleAddPedido(product)} >  
-          //     <h1>Atendente: ${name}</h1>
-          //     <h2>Cliente: ${cliente} - Mesa: ${mesa}</h2>
-          //     <p> {localStorage.getItem('product.id')} {localStorage.getItem('product.price')} </p> 
-          //   </div>    
-          //   )
-
-          // })
-
-          // itemPedido.map((product) => {
-          //   return (
-          //     <div className='requests' key={`product-${Math.random()}`} > 
-          //       <p className='white-text' key={Math.random()}>{product.name}</p> 
-          //       <p className='white-text'>R$ {product.price},00</p> 
-          //     </div>    
-          //   )
-        
-          // })
-        }
-        
-        {/* <div className='show-total'>
+        <div className='show-total'>
           <p>TOTAL R$ {localStorage.getItem('valueTotal')}</p>
-        </div> */}
+        </div>
 
-        {/* <Button 
+        <Button 
           className='button'
           name='Adicionar'
           type='submit'
-          onClick={(event) => HandleAddPedido(event)}       
-        /> */}
+          onClick= {handleClick}
+        />
 
-        {/* <Button
-          className='button'
-          name='Adicionar'
-          type='submit'
-          onClick={(e) => {
-            const product = e.target.parentNode;
-            const idProduct = product.getAttribute('id')
-            const nameProduct = product.getAttribute('name')
-            const priceProduct = product.getAttribute('price')
-
-            const pedido = {
-              id: idProduct,
-              name: nameProduct,
-              price: priceProduct,
-            }
-            HandlePedido(pedido)
-            console.log(HandlePedido)
-          }}
-        /> */}
-
-      {/* </div> */}
+      </div>     
+       
     </div>
   )
 }
--- src/pages/orderMenu/orderMenu.js
+++ src/pages/orderMenu/orderMenu.js
@@ -1,118 +1,89 @@
 import React, { useState } from 'react';
-//import { useHistory } from "react-router-dom";
 import Header from '../../components/header/innerHeader';
 import Button from '../../components/button';
-import Input from '../../components/input';
+//import Input from '../../components/input';
 import Breakfast from '../../components/cardapio/breakfast';
 import Allday from '../../components/cardapio/allday';
 
 const OrderMenu = () => {
-  // const history = useHistory();
-
-  // const routerReadyOrders = () => {
-  //   history.push('/ready-orders')
-  // }
-
-  // const routerFinalizedOrders = () => {
-  //   history.push('/finalized-orders')
-  // }
-
-
-  // const [userCliente, setUserCliente] = useState(''); 
-  // const [userMesa, setUserMesa] = useState(''); 
   
-  // const HandleUserCliente = (e) => {
-  //   setUserCliente(e.target.value);
-  // };
-
-  // const HandleUserMesa = (e) => {
-  //   setUserMesa(e.target.value);
-  // };
-
-
-
-  // const handleChange = (e) => {
-  //   this.setState({value: e.target.value});
-  // }
-
-  // const [extras, setExtras] = useState([])
-
-  // const handleExtras = (e) => {
-  //   setExtras(e.target.value);
-  // };
- 
   const [menuSelect, setMenuSelect] = useState ('Selecione o Menu')
 
   const HandleMenuSelect = (e) => {
   setMenuSelect(e.target.value);
   };
+   
+  //const [userCliente, setUserCliente] = useState(''); 
+  //const [userMesa, setUserMesa] = useState(''); 
+  // const cliente = localStorage.setItem('userCliente', userCliente)
+  // const mesa = localStorage.setItem('userMesa', userMesa)
+  // const nameAtendente = localStorage.getItem('userName')
+  // const [order, setOrder] = useState([])
  
-  // const [finishOrder, setFinishOrder] = useState ([])
+ 
+  //const [itemPedido, setItemPedido] = useState([]);
+  //const [valorTotal, setValorTotal] = useState(0);
 
-  // const HandleFinishOrder = (e) => {
-  // setFinishOrder(e.target.value);
+
+  // function handleClick(product) {
+  //   console.log(product.id);
+  //   const obj = {
+  //     id: product.id,
+  //     qtd:0,
+  //   }
+       
+  //   setOrder((prevState) => [...prevState, obj]);
   // }
 
+  // setItems(
+  //   items.map((item, index) => {
+  //     item.id === id ? newItem : item
+  //   })
+  // )
 
 
+
+
+  // const HandleAddPedido = (product) => {
+  //   localStorage.getItem('userName')
+  //   localStorage.getItem('userCliente')
+  //   localStorage.getItem('userMesa')
+  //   const addProduct = itemPedido
+  //   addProduct.push(product)
+  //   setItemPedido(addProduct);
+  //   HandleSomaValor()
+  // }
+  // console.log(itemPedido)
   
-  const [userCliente, setUserCliente] = useState(''); 
-  const [userMesa, setUserMesa] = useState(''); 
-  const cliente = sessionStorage.setItem('userCliente', userCliente)
-  const mesa = sessionStorage.setItem('userMesa', userMesa)
-  const nameAtendente = localStorage.getItem('userName')
-
-  const HandleUserCliente = (e) => {
-    setUserCliente(e.target.value);
-  };
-
-  const HandleUserMesa = (e) => {
-    setUserMesa(e.target.value);
-  };
   
-  const [itemPedido, setItemPedido] = useState([]);
-  const [valorTotal, setValorTotal] = useState(0);
 
-  const HandleAddPedido = (product) => {
-    localStorage.getItem('userName')
-    sessionStorage.getItem('userCliente')
-    sessionStorage.getItem('userMesa')
-    const addProduct = itemPedido
-    addProduct.push(product)
-    setItemPedido(addProduct);
-    HandleSomaValor()
-  }
-  console.log(itemPedido)
-  
  
-  const HandleSomaValor = () => {
-    itemPedido.forEach(product => {
-      const valorInicial = Number(product.price)
-      //valorTotal.reduce((valorInicial, valorAdd) => valorInicial + valorAdd, 0)
-      setValorTotal(valorInicial + valorTotal)
-    }) 
+  // const HandleSomaValor = () => {
+  //   itemPedido.forEach(product => {
+  //     const valorInicial = Number(product.price)
+  //     //valorTotal.reduce((valorInicial, valorAdd) => valorInicial + valorAdd, 0)
+  //     setValorTotal(valorInicial + valorTotal)
+  //   }) 
         
-    // itemPedido.forEach(item => {
-    //   const valor = Number(item.price)
-    //   setValorTotal(valor + valorTotal)
-    // })
-  }
+  // }
 
-  console.log(HandleSomaValor)
+  // console.log(HandleSomaValor)
 
   // const HandlePedido = (e) => {
   //   e.preventDefault();
   //   sessionStorage.getItem('userCliente')
   //   sessionStorage.getItem('userMesa')
   // }
+  
 
+  
 
 
   return (
     <div className='order'>
       <div className='order-menu'>
         <Header />
-        <section>
+        {/* <section>
           <Input
             required
             className='input'
@@ -120,7 +91,7 @@ const OrderMenu = () => {
             type='text'
             placeholder='Nome do Cliente'
             value={userCliente}
-            onChange={HandleUserCliente}
+            onChange={(e) => setUserCliente (e.target.value)}
           />
           <Input
             required
@@ -129,9 +100,9 @@ const OrderMenu = () => {
             type='text'
             placeholder='Número da Mesa'
             value={userMesa}
-            onChange={HandleUserMesa}
+            onChange={(e) => setUserMesa (e.target.value)}
           />
-        </section>
+        </section> */}
 
         <select className='yellow-text' id='menu' value={menuSelect} onChange= {HandleMenuSelect}>
           <option value='Selecione o Menu' disabled defaultValue>Selecione o Menu</option>
@@ -152,70 +123,15 @@ const OrderMenu = () => {
           }
         </>
 
-      </div>
+      </div>  
 
-      <div className='order-resume'>  
-      
-        <p>RESUMO DO PEDIDO</p>
+      <Button 
+        className='button'
+        name='Finalizar Pedido'
+        type='submit'
+        onClick= {() => { }}
+      />
 
-        {
-          itemPedido.map((product) => (
-              <div className='resume' key={`product-${product.id}`}> 
-                <h1>Atendente: {nameAtendente}</h1>
-                <h2>Cliente: {userCliente} - Mesa: {userMesa}</h2>
-                <p> {localStorage.getItem('product.id')} {localStorage.getItem('product.price')} </p>               
-              </div>    
-            )
-          )
-        }  
-
-        {/* <section>
-          <Input
-            required
-            className='input'
-            name='userCliente'
-            type='text'
-            placeholder='Nome do Cliente'
-            value={userCliente}
-            onChange={HandleUserCliente}
-          />
-          <Input
-            required
-            className='input'
-            name='userMesa'
-            type='text'
-            placeholder='Número da Mesa'
-            value={userMesa}
-            onChange={HandleUserMesa}
-          />
-        </section> */}
-
-        
-        <div className='show-total'>
-          <p>TOTAL R$ {localStorage.getItem('valueTotal')}</p>
-        </div>
-
-        <Button 
-          className='button'
-          name='Adicionar'
-          type='submit'
-          onClick= {(e) => {
-            const product = e.target.parentNode.parentNode;
-            const idProduct = product.getAttribute('id')
-            const nameProduct = product.getAttribute('name')
-            const priceProduct = product.getAttribute('price')
-
-            const pedido = {
-              id: idProduct,
-              name: nameProduct,
-              price: priceProduct,
-            }
-            HandleAddPedido(pedido)
-            console.log(HandleAddPedido)
-          }}      
-        />
-
-      </div>
     </div>  
   )
 }
