2907333baffbff16c4140b8b33843edb268b3743
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-03-11 11:08:19 -0300

    correção mudança da cor do menu ativo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,64 +1,54 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
-import Input from '../input';
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  const history = useHistory();
   const userRole = localStorage.getItem("userRole");
 
-  const handleNavigate = (event, path) => {
-    event.preventDefault()
-    history.push(path)
-  }
-
   return (
-    <div className='navbar'>
-      <section className='hall-navbar'>
+    <nav className='navbar'>
+      <div className='hall-navbar'>
         {userRole === "salão" ?
           (
             <>
-            <Input
-              // checked='checked'
-              className='navbar-hall'
-              id='order-menu'
-              type='radio'
-              name='pages'
-              value='/order-menu'
-              onChange={(event) => handleNavigate(event, event.target.value)}
-            />
-            <label htmlFor='order-menu'>Menu pedidos</label>
-            <Input
-              id='ready-orders'
-              type='radio'
-              name='pages'
-              value='/ready-orders'
-              onChange={(event) => handleNavigate(event, event.target.value)}
-            />
-            <label htmlFor='ready-orders'>Pedidos prontos</label>
-          </>
-        ) : (
+              <NavLink
+                className="navbar-link"
+                activeClassName="active"
+                to="/order-menu"
+              >
+                Menu pedidos
+              </NavLink>
+
+              <NavLink
+                className="navbar-link"
+                activeClassName="active"
+                to="/ready-orders"
+              >
+                Pedidos prontos
+              </NavLink>
+            </>
+          ) : (
           <>
-            <Input
-              id='pending-orders'
-              type='radio'
-              name='pages'
-              value='/pending-orders'
-              onChange={(event) => handleNavigate(event, event.target.value)}
-            />
-            <label htmlFor='pending-orders'>Pedidos pendentes</label>
+            <NavLink
+              className="navbar-link"
+              activeClassName="active"
+              to="/pending-orders"
+            >
+              Pedidos pendentes
+            </NavLink>
           </>
-        )
-      }
-      <Input
-        id='finalized-orders'
-        type='radio'
-        name='pages'
-        value='/finalized-orders'
-        onChange={(event) => handleNavigate(event, event.target.value)}
-      />
-      <label htmlFor='finalized-orders'>Pedidos finalizados</label>
-      </section>
-    </div>
+          )
+        }
+
+        <NavLink
+          className="navbar-link"
+          activeClassName="active"
+          to="/finalized-orders"
+        >
+          Pedidos finalizados
+        </NavLink>
+
+      </div>
+    </nav>
   )
 }
 
